758f607e12929be9ac6e93ab234d194659b016b9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-10-13 14:16:19 +0800

    [doc] GoDoc for Extend Block

--- block.go
+++ block.go
@@ -4,16 +4,19 @@ import (
 	"github.com/llir/llvm/ir"
 )
 
-type extBlock struct {
+// Block receives a ir.Block and returns its extension
+func Block(b *ir.Block) *ExtBlock {
+	return &ExtBlock{
+		Block: b,
+	}
+}
+
+// ExtBlock is the extension of ir.Block
+type ExtBlock struct {
 	*ir.Block
 }
 
-func (e *extBlock) HasTerminator() bool {
+// HasTerminator checks the end of Block is a terminator or not
+func (e *ExtBlock) HasTerminator() bool {
 	return e.Term != nil
-}
-
-func Block(b *ir.Block) *extBlock {
-	return &extBlock{
-		Block: b,
-	}
 }
