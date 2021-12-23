22881cdc5530c749afc3c0fb07546de13ea92b4c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-23 17:44:27 +0800

    we don't need phi test now, we separate them

--- block_test.go
+++ block_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/dannypsnl/extend"
 	"github.com/llir/llvm/ir"
-	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,16 +24,4 @@ func TestExtBlock_BelongsToFunc_True(t *testing.T) {
 func TestExtBlock_BelongsToFunc_False(t *testing.T) {
 	b := extend.Block(ir.NewBlock(""))
 	assert.False(t, b.BelongsToFunc())
-}
-
-func TestExtBlock_PhiInStartAutomatically(t *testing.T) {
-	b := extend.Block(ir.NewBlock(""))
-	x := b.NewAdd(constant.NewInt(types.I1, 1), constant.NewInt(types.I1, 2))
-	y := b.NewAdd(constant.NewInt(types.I1, 1), constant.NewInt(types.I1, 2))
-	b.NewPhi(
-		ir.NewIncoming(x, b.Block),
-		ir.NewIncoming(y, b.Block),
-	)
-	assert.NotNil(t, b.Block.Insts[0].(*ir.InstPhi))
-	assert.NotNil(t, b.Block.Insts[1].(*ir.InstAdd))
 }
